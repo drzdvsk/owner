638fa158e1e2485cee0ede98fd71780716208981
Andrei Drazdouski <a.drzdvsk@gmail.com>
2024-11-07 04:22:26 +0300

    add TypeConfig

---
+++ src/test/java/tests/config/TypeConfig.java
@@ -0,0 +1,23 @@
+package tests.config;
+
+import org.aeonbits.owner.Config;
+
+public interface TypeConfig extends Config {
+
+    @Key("integer")
+    Integer getInteger();
+
+    @Key("double")
+    Double  getDouble();
+
+    @Key("boolean")
+    Boolean  getBoolean();
+
+    @Key("enum")
+    Browser getEnum();
+
+
+
+
+
+}
